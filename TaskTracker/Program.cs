13105415d5f3a70d3f8d6c721fe370228cce5db8
Melissa Hegney <mhegney@stmartin.edu>
2026-01-11 12:44:52 -0800

    project scaffolding

---
+++ TaskTracker/Program.cs
@@ -0,0 +1,12 @@
+
+Console.WriteLine("Welcome to TaskTracker!");
+Console.WriteLine("1. List Tasks");
+Console.WriteLine("2. About");
+Console.Write("Choose an option: ");
+
+var input = Console.ReadLine();
+
+if (input == "2")
+{
+    Console.WriteLine("Replace this with your About implementation.");
+}
